6bb4dbd776e9dec3ea876ba1dffd20f9c67805f5
sljeff <kindjeff.com@gmail.com>
2024-07-03 12:30:47 +0800

    fix(date format): support zh_CN

--- lib/util/datetime.dart
+++ lib/util/datetime.dart
@@ -1,10 +1,6 @@
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'timezone.dart';
-
-/// The `Z` part is not yet implemented according to https://pub.dev/documentation/intl/latest/intl/DateFormat-class.html
-/// We will remove it for now and parse the timezone separately.
-const rfc822DatePattern = 'EEE, dd MMM yyyy HH:mm:ss';
-final rfc822DateFormat = DateFormat(rfc822DatePattern, 'en_US');
 
 DateTime? parseDateTime(dateString) {
   if (dateString == null) return null;
@@ -15,15 +11,28 @@ DateTime? parseDateTime(dateString) {
 /// We will parse the date string as UTC and then
 /// subtract the actual time offset from the parsed string.
 DateTime? _parseRfc822DateTime(String dateString) {
-  try {
-    final localTime = rfc822DateFormat.parse(dateString, true);
-    final timezone = dateString.trim().split(' ').last;
+  initializeDateFormatting('zh_CN', null);
 
-    final timeOffset = Duration(minutes: getTimeZoneOffset(timezone) ?? 0);
-    return localTime.subtract(timeOffset).toUtc();
-  } on FormatException {
-    return null;
+  /// The `Z` part is not yet implemented according to https://pub.dev/documentation/intl/latest/intl/DateFormat-class.html
+  /// We will remove it for now and parse the timezone separately.
+  const rfc822DatePattern = 'EEE, dd MMM yyyy HH:mm:ss';
+  final rfc822DateFormats = [
+    DateFormat(rfc822DatePattern, 'en_US'),
+    DateFormat(rfc822DatePattern, 'zh_CN'),
+  ];
+
+  for (var rfc822DateFormat in rfc822DateFormats) {
+    try {
+      final localTime = rfc822DateFormat.parse(dateString, true);
+      final timezone = dateString.trim().split(' ').last;
+
+      final timeOffset = Duration(minutes: getTimeZoneOffset(timezone) ?? 0);
+      return localTime.subtract(timeOffset).toUtc();
+    } on FormatException {
+      continue;
+    }
   }
+  return null;
 }
 
 DateTime? _parseIso8601DateTime(dateString) {
